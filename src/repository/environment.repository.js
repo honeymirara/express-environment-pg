6c669aeb8af44ffa5f208cc12978afdc1e8cc492
honeymirara <honeymirara@gmail.com>
2023-05-13 13:54:58 -1000

    getAllDataDB

--- src/repository/environment.repository.js
+++ src/repository/environment.repository.js
@@ -1,8 +1,10 @@
 const pool = require("../db");
 
-async function getAllData() {
+async function getAllDataDB() {
     const client = await SecurityPolicyViolationEvent.connect()
     const sql = `SELECT * FROM environment`
     const result = (await client.query(sql)).rows;
     return result;
-}
+};
+
+module.exports = {getAllDataDB}
